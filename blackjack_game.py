9bb74234fdb74eddd0d8559a2e7d325d5331424f
LukeGarbutt <luke_garbutt@live.co.uk>
2017-07-08 00:21:59 +0100

    initial file with brief structure

---
+++ blackjack_game.py
@@ -0,0 +1,13 @@
+# main file
+def main():
+	pass # this function will call our individual modules probably on a loop to play blackjack
+
+# create deck class
+
+# create card class
+
+# create game rules and conditions
+
+# create player class
+
+# create dealer class
