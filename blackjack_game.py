d587f4d56c2fd4aaccbab64079eda4c55bf6dddb
LukeGarbutt <luke_garbutt@live.co.uk>
2017-07-08 01:22:18 +0100

    test

--- blackjack_game.py
+++ blackjack_game.py
@@ -2,25 +2,25 @@
 import random
 
 def main(): # this function will call our individual modules probably on a loop to play blackjack
-	deck = initialise_deck() # list of 52 card objects, all unique suit and value
-	deck.shuffle() # deck method that will shuffle the cards
-	for i in deck:
-		print(i.value, i.suit)
+	deck1 = deck() # list of 52 card objects, all unique suit and value
+	print(deck1.list_of_cards)
+	deck1.shuffle() # deck method that will shuffle the cards
+	print(deck1.list_of_cards[1])
 
 # create deck class
 
 class deck():
 	def __init__(self):
-		self.deck = add_cards()
+		self.add_cards()
 
 	def shuffle(self):
-		self.deck = random.shuffle(self.deck)
+		self.list_of_cards = random.shuffle(self.list_of_cards)
 
 	def add_cards(self):
-		deck = []
+		self.list_of_cards = []
 		for i in range(1, 53):
-			deck.append(card(i))
-		return(deck)
+			self.list_of_cards.append(card(i))
+		
 
 
 # create card class
@@ -30,6 +30,22 @@ class deck():
 # create player class
 
 # create dealer class
+
+class card():
+	def __init__(self, id):
+		self.init_val(id)
+		self.init_suit(id)
+
+	def init_suit(self, id):
+		suits = ['Spades', 'Clubs', 'Hearts', 'Diamonds']
+		id = id%4
+		self.suit = suits[id]
+
+	def init_val(self, id):
+		values = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
+		id = id%13
+		self.value = values[id]
+
 
 if __name__ == '__main__':
 	main()
